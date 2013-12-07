e6fb2165381a6e5d4f93f8daa121f6e2d2fc4850
matutn <fausti.matias@gmail.com>
2013-12-07 20:04:08 -0300

    ...

---
+++ Aplicacion Desktop/Clinica Frba/Login/SelecFunc.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Clinica_Frba.Login
+{
+    partial class SelecFunc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.profesionalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.afiliadoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.administrativoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pedirTurnoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.compraDeBonosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cancelarAtenciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abmAfiliadoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abmProfesionalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abmRolToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cancelarAtenciónToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.registroDeLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registroResultadoAtenciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cancelarAtenciónToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.registrarAgendaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registroDeLlegadaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.profesionalToolStripMenuItem,
+            this.afiliadoToolStripMenuItem,
+            this.administrativoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(292, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // profesionalToolStripMenuItem
+            // 
+            this.profesionalToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cancelarAtenciónToolStripMenuItem2,
+            this.registrarAgendaToolStripMenuItem});
+            this.profesionalToolStripMenuItem.Name = "profesionalToolStripMenuItem";
+            this.profesionalToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.profesionalToolStripMenuItem.Text = "Profesional";
+            this.profesionalToolStripMenuItem.Click += new System.EventHandler(this.profesionalToolStripMenuItem_Click);
+            // 
+            // afiliadoToolStripMenuItem
+            // 
+            this.afiliadoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pedirTurnoToolStripMenuItem,
+            this.compraDeBonosToolStripMenuItem,
+            this.cancelarAtenciónToolStripMenuItem});
+            this.afiliadoToolStripMenuItem.Name = "afiliadoToolStripMenuItem";
+            this.afiliadoToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.afiliadoToolStripMenuItem.Text = "Afiliado";
+            // 
+            // administrativoToolStripMenuItem
+            // 
+            this.administrativoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abmAfiliadoToolStripMenuItem,
+            this.abmProfesionalToolStripMenuItem,
+            this.abmRolToolStripMenuItem,
+            this.cancelarAtenciónToolStripMenuItem1,
+            this.registroDeLToolStripMenuItem,
+            this.registroResultadoAtenciónToolStripMenuItem,
+            this.registroDeLlegadaToolStripMenuItem});
+            this.administrativoToolStripMenuItem.Name = "administrativoToolStripMenuItem";
+            this.administrativoToolStripMenuItem.Size = new System.Drawing.Size(87, 20);
+            this.administrativoToolStripMenuItem.Text = "Administrativo";
+            // 
+            // pedirTurnoToolStripMenuItem
+            // 
+            this.pedirTurnoToolStripMenuItem.Name = "pedirTurnoToolStripMenuItem";
+            this.pedirTurnoToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.pedirTurnoToolStripMenuItem.Text = "Pedir Turno";
+            // 
+            // compraDeBonosToolStripMenuItem
+            // 
+            this.compraDeBonosToolStripMenuItem.Name = "compraDeBonosToolStripMenuItem";
+            this.compraDeBonosToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.compraDeBonosToolStripMenuItem.Text = "Compra de Bonos";
+            // 
+            // cancelarAtenciónToolStripMenuItem
+            // 
+            this.cancelarAtenciónToolStripMenuItem.Name = "cancelarAtenciónToolStripMenuItem";
+            this.cancelarAtenciónToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.cancelarAtenciónToolStripMenuItem.Text = "Cancelar Atención";
+            // 
+            // abmAfiliadoToolStripMenuItem
+            // 
+            this.abmAfiliadoToolStripMenuItem.Name = "abmAfiliadoToolStripMenuItem";
+            this.abmAfiliadoToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.abmAfiliadoToolStripMenuItem.Text = "Abm Afiliado";
+            this.abmAfiliadoToolStripMenuItem.Click += new System.EventHandler(this.abmAfiliadoToolStripMenuItem_Click);
+            // 
+            // abmProfesionalToolStripMenuItem
+            // 
+            this.abmProfesionalToolStripMenuItem.Name = "abmProfesionalToolStripMenuItem";
+            this.abmProfesionalToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.abmProfesionalToolStripMenuItem.Text = "Abm Profesional";
+            this.abmProfesionalToolStripMenuItem.Click += new System.EventHandler(this.abmProfesionalToolStripMenuItem_Click);
+            // 
+            // abmRolToolStripMenuItem
+            // 
+            this.abmRolToolStripMenuItem.Name = "abmRolToolStripMenuItem";
+            this.abmRolToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.abmRolToolStripMenuItem.Text = "Abm Rol";
+            this.abmRolToolStripMenuItem.Click += new System.EventHandler(this.abmRolToolStripMenuItem_Click);
+            // 
+            // cancelarAtenciónToolStripMenuItem1
+            // 
+            this.cancelarAtenciónToolStripMenuItem1.Name = "cancelarAtenciónToolStripMenuItem1";
+            this.cancelarAtenciónToolStripMenuItem1.Size = new System.Drawing.Size(172, 22);
+            this.cancelarAtenciónToolStripMenuItem1.Text = "Cancelar Atención";
+            // 
+            // registroDeLToolStripMenuItem
+            // 
+            this.registroDeLToolStripMenuItem.Name = "registroDeLToolStripMenuItem";
+            this.registroDeLToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.registroDeLToolStripMenuItem.Text = "Listados Estadísticos";
+            this.registroDeLToolStripMenuItem.Click += new System.EventHandler(this.registroDeLToolStripMenuItem_Click);
+            // 
+            // registroResultadoAtenciónToolStripMenuItem
+            // 
+            this.registroResultadoAtenciónToolStripMenuItem.Name = "registroResultadoAtenciónToolStripMenuItem";
+            this.registroResultadoAtenciónToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.registroResultadoAtenciónToolStripMenuItem.Text = "Registro Resultado Atención";
+            // 
+            // cancelarAtenciónToolStripMenuItem2
+            // 
+            this.cancelarAtenciónToolStripMenuItem2.Name = "cancelarAtenciónToolStripMenuItem2";
+            this.cancelarAtenciónToolStripMenuItem2.Size = new System.Drawing.Size(172, 22);
+            this.cancelarAtenciónToolStripMenuItem2.Text = "Cancelar Atención";
+            // 
+            // registrarAgendaToolStripMenuItem
+            // 
+            this.registrarAgendaToolStripMenuItem.Name = "registrarAgendaToolStripMenuItem";
+            this.registrarAgendaToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.registrarAgendaToolStripMenuItem.Text = "Registrar Agenda";
+            // 
+            // registroDeLlegadaToolStripMenuItem
+            // 
+            this.registroDeLlegadaToolStripMenuItem.Name = "registroDeLlegadaToolStripMenuItem";
+            this.registroDeLlegadaToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.registroDeLlegadaToolStripMenuItem.Text = "Registro de Llegada";
+            // 
+            // SelecFunc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 241);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "SelecFunc";
+            this.Text = "SelecFunc";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem profesionalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem afiliadoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pedirTurnoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem compraDeBonosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cancelarAtenciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem administrativoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abmAfiliadoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abmProfesionalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abmRolToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cancelarAtenciónToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem registroDeLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cancelarAtenciónToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem registrarAgendaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registroResultadoAtenciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registroDeLlegadaToolStripMenuItem;
+    }
+}
---
+++ Aplicacion Desktop/Clinica Frba/Login/SelecFunc.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Clinica_Frba.GrillaAfiliado;
+using Clinica_Frba.GrillaProfesional;
+using Clinica_Frba.GrillaRol;
+
+
+namespace Clinica_Frba.Login
+{
+    public partial class SelecFunc : Form
+    {
+
+
+        public SelecFunc()
+        {
+            InitializeComponent();
+
+            Ingreso ingreso = new Ingreso();
+            if (ingreso.ShowDialog() != DialogResult.OK)
+                this.Close();
+
+
+
+        }
+
+        private void registroDeLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void profesionalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void abmAfiliadoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            (new GrillaAfiliado_Form()).Show(); 
+        }
+
+        private void abmProfesionalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            (new GrillaProfesional_Form()).Show();
+        }
+
+        private void abmRolToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            (new GrillaRol_Form()).Show();
+        }
+    }
+}
